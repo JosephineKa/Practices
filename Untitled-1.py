bb19bbe6eeb9a1ce8829a8e6f8e4bc657d451c3f
JosephineKa <kamwelajosephinek@gmail.com>
2026-02-05 19:18:05 -0600

    Create Untitled-1.py
    practing working vscode and github

---
+++ Untitled-1.py
@@ -0,0 +1,7 @@
+print("Hello, world!");
+print("Trying to work with github and vs code at the same time");
+print("This is my third line of code.");
+print("understanding how github and vs code works together");
+print("I am using python to write my code");
+print("I would like to learn more on working with vs code and github");
+print("This is my last line of code for today.");
